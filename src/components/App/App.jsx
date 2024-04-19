049e245e0f62f8190516bf8127913bbf64cdfd3f
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-04-19 15:38:19 +0300

    add routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,17 @@ import { lazy } from "react";
 import Layout from '../Layout/Layout';
 
 const HomePage = lazy(() => import('../../pages/HomePage'));
+const CatalogPage = lazy(() => import('../../pages/CatalogPage'));
+const FavoritePage = lazy(() => import('../../pages/FavoritePage'));
 
 export const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Layout/>}>
-                <Route index element={<HomePage/>}/>
+                <Route index element={<HomePage />} />
+                <Route path='/catalog' element={<CatalogPage />} />
+                <Route path='/favorites' element={<FavoritePage />} />
+                <Route path="*" element={<HomePage />} />
             </Route>
         </Routes>
     )
