28d6df00e979abe75d44a9edea41e8048644e7f9
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-04-19 16:00:22 +0300

    add pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,42 +1,14 @@
-import { useState, useEffect } from 'react';
+
 import { Main, Title } from './Pages.styled';
-import Loader from '../components/Loader/Loader';
-import AdvertsList from '../components/AdvertsList/AdvertsList';
-import { fetchAdverts } from 'services/api';
 
 const HomePage = () => {
-    const [adverts, setAdverts] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setIsLoading(true);
-                const advertsData = await fetchAdverts();
-                setAdverts(advertsData);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []);
+  
 
     return (
         <Main>
             <div>
-                {isLoading && <Loader />}
-                <Title>Adverts</Title>
-                {error ? (
-                    <p>Error: {error.message}</p>
-                ) : adverts !== 0 ? (
-                    <AdvertsList adverts={adverts} />
-                ) : (
-                    !isLoading && <p>No adverts found.</p> // Додана умова для перевірки isLoading
-                )}
+                <Title>Home Page</Title>
+                <p>This page is...</p>
             </div>
         </Main>
     );
