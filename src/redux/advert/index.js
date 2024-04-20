20b1c14326030c22db1873dfcd9c4e2757c69432
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-04-20 22:00:43 +0300

    create index.js

---
+++ src/redux/advert/index.js
@@ -0,0 +1,3 @@
+export { default as advertsReducer } from './advert-slice';
+export { default as advertsOperations } from './advert-operations';
+export { default as advertsSelector } from './advert-selectors';
